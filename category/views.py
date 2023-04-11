91df770fa78a5385313044df72bff0d8c7a70f9d
XorysDev <xorysteam.dev@gmail.com>
2023-04-11 12:23:11 +0600

    add category

--- category/views.py
+++ category/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from category.models import Category
+from . import serializers
 
-# Create your views here.
+
+class CategoryCreateListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return permissions.IsAdminUser(),
+
+        return permissions.AllowAny(),
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = (permissions.IsAdminUser,)
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return permissions.AllowAny(),
+
+        return permissions.IsAdminUser(),
