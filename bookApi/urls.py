6426032a298da7820c9cda6951a7584a825044e6
XorysDev <xorysteam.dev@gmail.com>
2023-04-14 00:05:32 +0600

    changed name swagger, new A mini-feature for send_mail

--- bookApi/urls.py
+++ bookApi/urls.py
@@ -8,9 +8,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Blog API test",
+        title="BOOK API v1",
         default_version='v1',
-        description="test api for blog api py.26",
+        description="Swagger api, is a support tool for the frontend team",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
